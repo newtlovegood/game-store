637f60f0f319d5ce22cde5ca54526a95b0b33e2f
wynona <wynonagoldman@yahoo.com>
2021-04-17 11:55:43 +0300

    added a users app

--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'games'
 urlpatterns = [
-    path('', views.HomePageView.as_view())
+    path('', views.HomePageView.as_view()),
+    path('games/<int:pk>/', views.SingleGameView.as_view(), name='detail'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,7 +1,26 @@
+from django.utils import timezone
+
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
+from games.models import Game
 
 
 class HomePageView(TemplateView):
+
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['games'] = Game.objects.all()[:5]
+        return context
+
+
+class SingleGameView(DetailView):
+
+    model = Game
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
 
