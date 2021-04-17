e4d978608a0bf179867772c026c22fcec191450e
wynona <wynonagoldman@yahoo.com>
2021-04-17 15:38:20 +0300

    login/signup basic added

--- game_store_epam/settings.py
+++ game_store_epam/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CUSTOM CONSTANTS
+
+LOGIN_REDIRECT_URL = '/'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import models
 
-# Create your models here.
+
+class User(models.User, models.PermissionsMixin):
+
+    def __str__(self):
+        return f"self.username"
+
+
+class Manager(User):
+    pass
+
+
